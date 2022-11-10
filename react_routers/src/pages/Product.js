5265327bf66552b9cb92fb4607affcc21be7bf94
Bruno Alves <bruno6821@gmail.com>
2022-11-10 19:02:33 -0300

    Implementando detalhes do Produto

--- react_routers/src/pages/Product.js
+++ react_routers/src/pages/Product.js
@@ -1,10 +1,26 @@
 import { useParams } from "react-router-dom";
+import {useFetch} from "../hooks/useFetch"
 
 const Product = () => {
+  // capturando dados da URL
   const { id } = useParams();
+
+  const url = "http://localhost:3000/products/" + id
+
+  // Carregamento de dado Individual 
+  const { data: product, loading, error} = useFetch(url)
 
   return <>
   <p>ID DO PRODUTO {id}</p>
+  {error && <p>Ocorreu um Erro...</p>}
+  {loading && <p>Carregando...</p> }
+  {product && (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.price}</p>
+    </div>
+    )
+  }
   </>;
 };
 
