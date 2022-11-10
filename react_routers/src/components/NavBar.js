1d94864b7a5d6a7b17c1ccf99e8312bd31ace24b
Bruno Alves <bruno6821@gmail.com>
2022-11-10 19:22:07 -0300

    Implementando o Link Ativo com o NavLink

--- react_routers/src/components/NavBar.js
+++ react_routers/src/components/NavBar.js
@@ -1,12 +1,19 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
+      {/* 
       <Link to="/">Home</Link>
       <Link to="/About">Sobre</Link>
-      <Link to="/products/:id">Produtos</Link>
+      <Link to="/products/:id">Produtos</Link> 
+      */}
+
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/About">Sobre</NavLink>
+      <NavLink to="/products/:id">Produtos</NavLink> 
+
     </nav>
   );
 };
